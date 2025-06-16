f91e9a76b1a2ff44a522f6bbae7de4e2d0d98375
felipekttu <kttudev@gmail.com>
2025-06-16 08:44:02 -0300

    Aula 1 - EdN

---
+++ Aula1.py
@@ -0,0 +1,39 @@
+# Aula 1 - Escola da Nuvem
+# 1- Programa de Saudação
+# * Crie um programa que imprima a mensagem "Olá, mundo!" na tela.
+print("Olá, mundo!")
+
+# 2- Calculadora de Soma
+# * Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado.
+numero1 = 12
+numero2 = 12
+soma = numero1 + numero2
+print(f"O resultado da soma é: {soma}")
+
+# 3- Calculadora de Volume
+# * Crie um programa que calcula o volume de uma caixa retangular. Use as seguintes dimensões:
+# * Comprimento: 12 cm
+# * Largura: 14 cm
+# * Altura: 20 cm
+# O programa deve calcular o volume e exibir o resultado em cm³.
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print(f"O volume da caixa é de: {volume} cm³")
+
+# 4- Calculadora de Preço Total
+# * Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+# * Nome do produto: "Cadeira Infantil"
+# * Preço unitário: R$ 12.40
+# * Quantidade: 3
+# O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+nome_produto = "Cadeira Infantil"
+preco_unit = 12.40
+quantidade = 3
+preco_total = preco_unit * quantidade
+
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unit:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"O preço total da compra é: R$ {preco_total:.2f}")
