078c0a52c55f14d25d91233f449806269d8390cd
felipekttu <kttudev@gmail.com>
2025-06-25 19:16:51 -0300

    Aula 2 - EdN

---
+++ Aula2/CalculadoradeMediaEscolar.py
@@ -0,0 +1,14 @@
+# 3- Calculadora de Média Escolar
+# Dados de entrada
+nota1 = 7.5
+nota2 = 8.0
+nota3 = 6.5
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibição dos resultados
+print("\n### Calculadora de Média Escolar ###")
+print(f"Nota 1: {nota1:.1f}")
+print(f"Nota 2: {nota2:.1f}")
+print(f"Nota 3: {nota3:.1f}")
+print("-" * 25)
+print(f"Média Final: {media:.2f}")
